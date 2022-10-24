2e6fb929762da8449910fe30d1ffc4400ee4e116
sara4se <s.m.m.h.h.18@gmail.com>
2022-10-24 09:52:05 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Sara Alhumidi on 28/03/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
